57e55c01a2a825de970b7d9bfb00aa36fdd0a7ca
Dan <dan@Zipcoders-MacBook-Pro.local>
2022-02-25 19:53:13 -0500

    Final Version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name:");
+        String name = input.nextLine();
+        if ((name.equals("Alice")) || (name.equals ("Bob"))) {
+            System.out.print("Hello!");
+        }
+        else {
+            System.out.print("You are not welcome here! (Just kidding)");
+        }
     }
 }
